d399752f8d78fe4c25b62c549448dda4d12ec1ed
James Fowler <josiah.james.fowler@gmail.com>
2021-04-12 12:01:31 -0400

    nonRepeatSubstring

---
+++ 1_Sliding-Window/Practice/nonRepeatStrings.js
@@ -0,0 +1,19 @@
+const non_repeat_substring = str => {
+    let windowStart = 0,
+        maxLength = 0,
+        charIndexMap = {};
+
+    for (let windowEnd = 0; windowEnd < str.length; windowEnd++){
+        const rightChar = str[windowEnd];
+
+        if (rightChar in charIndexMap) {
+            windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1)
+        }
+
+        charIndexMap[rightChar] = windowEnd;
+
+        maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    }
+
+    return maxLength;
+}

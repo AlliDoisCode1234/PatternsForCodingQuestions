bedc44379ca197a9633e81df5bc9379f6562ce87
James Fowler <josiah.james.fowler@gmail.com>
2021-04-15 11:09:09 -0400

    bigO

--- 1_Sliding-Window/findWordCancat.js
+++ 1_Sliding-Window/findWordCancat.js
@@ -1,3 +1,5 @@
+// O(N * M * Len) time | O(M + N) space - where N is the number of chars in the given string, M is the total number of words, and Len is the length of a word
+
 const find_word_concatenation = (str, words) => {
     if (words.length === 0 || words[0].length === 0) {
         return [];
@@ -19,7 +21,7 @@ const find_word_concatenation = (str, words) => {
     for (let i = 0; i < (str.length = wordsCount * wordLength) + 1; i++) {
         const wordsSeen = {};
         for (let j = 0; j < wordsCount; j++) {
-            next_word_index = i + j * wordLength;
+            let next_word_index = i + j * wordLength;
             // Get the next word from the string
             word = str.substring(next_word_index, next_word_index + wordLength);
             if (!(word in wordFrequency)) { // Break if we don't need this word

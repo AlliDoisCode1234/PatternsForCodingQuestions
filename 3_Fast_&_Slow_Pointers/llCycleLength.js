4e38b162d419ac7939361b0f38061a549deb52e3
James Fowler <josiah.james.fowler@gmail.com>
2021-04-16 11:04:10 -0400

    linkedListCycleLength

---
+++ 3_Fast_&_Slow_Pointers/llCycleLength.js
@@ -0,0 +1,36 @@
+class Node {
+    constructor(value, next = null) {
+        this.vale = value;
+        this.next = next;
+    }
+}
+
+// O(N) time | O(1) space - where N is the length of the linkedList
+
+const find_cycle_length = head => {
+    let slow = head,
+        fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (slow === fast) {
+            return calculate_cycle_length(slow);
+        }
+    }
+    return 0;
+}
+
+
+const calculate_cycle_length = slow => {
+    let current = slow,
+        cycle_length = 0;
+    while (true) {
+        current = current.next;
+        cycle_length += 1;
+        if (current === slow) {
+            break;
+        }
+    }
+    return cycle_length;
+}

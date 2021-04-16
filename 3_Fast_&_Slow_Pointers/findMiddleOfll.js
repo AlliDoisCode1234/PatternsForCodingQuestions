edbf009eda32a727a44ad4dbe888feb3ffe3ab70
James Fowler <josiah.james.fowler@gmail.com>
2021-04-16 11:49:11 -0400

    spaceTime

--- 3_Fast_&_Slow_Pointers/findMiddleOfll.js
+++ 3_Fast_&_Slow_Pointers/findMiddleOfll.js
@@ -5,7 +5,7 @@ class Node {
     }
 }
 
-
+// O(N) time | O(1) space - where N is the length of the linked list
 const find_middle_of_linked_list = head => {
     let slow = head,
         fast = head;
@@ -17,3 +17,19 @@ const find_middle_of_linked_list = head => {
 
     return slow;
 }
+
+
+
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`);
+
+head.next.next.next.next.next = new Node(6);
+console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`);
+
+head.next.next.next.next.next.next = new Node(7);
+console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`);

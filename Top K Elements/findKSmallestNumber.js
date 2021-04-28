f8cc470439d0d093f178c5b34da648936fe62ea3
Josiah <josiah.james.fowler@gmail.com>
2021-04-28 11:31:33 -0400

    timeSpace

--- Top K Elements/findKSmallestNumber.js
+++ Top K Elements/findKSmallestNumber.js
@@ -1,7 +1,12 @@
 const Heap = require('./collections/heap'); //http://www.collectionsjs.com
 
+
+// O(N * logK) time | O(K) space
+
+
+
 function find_Kth_smallest_number(nums, k) {
-    maxHeap = new Heap();
+    let maxHeap = new Heap();
     // put first k numbers in the max heap
     for (let i = 0; i < k; i++) {
         maxHeap.push(nums[i]);
@@ -19,3 +24,5 @@ function find_Kth_smallest_number(nums, k) {
     // the root of the heap has the Kth smallest number
     return maxHeap.peek();
 }
+
+console.log(`Kth smallest number is: ${find_Kth_smallest_number([1, 5, 12, 2, 11, 5], 3)}`);

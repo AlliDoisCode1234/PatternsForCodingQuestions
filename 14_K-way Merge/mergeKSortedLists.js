1fbb0157c7c22ff0f781628a0678625ad86ab1f4
Josiah <josiah.james.fowler@gmail.com>
2021-04-29 11:11:10 -0400

    mergeKSortedLists

---
+++ 14_K-way Merge/mergeKSortedLists.js
@@ -0,0 +1,38 @@
+const Heap = require('./collections/heap'); //http://www.collectionsjs.com
+
+class ListNode {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+function merge_lists(lists) {
+    const minHeap = new Heap([], null, ((a, b) => b.value - a.value));
+
+    // put the root of each list in the min heap
+    lists.forEach((a) => {
+        if (a !== null) {
+            minHeap.push(a);
+        }
+    });
+
+    // take the smallest(top) element from the min-heap and add it to the result
+    // if the top element has a next element add it to the heap
+    let resultHead = null,
+        resultTail = null;
+    while (minHeap.length > 0) {
+        const node = minHead.pop();
+        if (resultHead === null) {
+            resultHead = resultTail = node;
+        } else {
+            resultTail.next = node;
+            resultTail = resultTail.next;
+        }
+        if (node.next !== null) {
+            minHeap.push(node.next)
+        }
+    }
+    return resultHead;
+
+}

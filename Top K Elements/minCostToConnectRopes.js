6e088bd9c5ccf5bcc9c8b7cbb57f6405257c799c
Josiah <josiah.james.fowler@gmail.com>
2021-04-29 10:39:37 -0400

    minCostToConnectRopes

---
+++ Top K Elements/minCostToConnectRopes.js
@@ -0,0 +1,20 @@
+const Heap = require('./collections/heap'); //http://www.collectionsjs.com
+
+
+function minimum_cost_to_connect_ropes(ropesLengths) {
+    // add al ropes to the minHeap
+    const minHeap = new Heap(ropeLengths, null, ((a, b) => b - a));
+
+    // go through the values of the heap, in each step take top (i.e., lowest) rope lengths from the min heap
+    // connect them and push the result back to the min heap
+    // keep doing this until the heap is left with only one rope
+
+    let result = 0;
+    while (minHeap.length > 1) {
+        const tempCost = minHeap.pop() + minHeap.pop();
+        result += tempCost
+        minHeap.push(tempCost);
+    }
+
+    return result;
+}

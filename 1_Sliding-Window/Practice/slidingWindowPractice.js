ffa92adea3b6069759e3bdd81eca16ecb42eac4e
Josiah <josiah.james.fowler@gmail.com>
2021-07-09 10:52:22 -0400

    sliding window practice

---
+++ 1_Sliding-Window/Practice/slidingWindowPractice.js
@@ -0,0 +1,16 @@
+function max_sub_array_of_size_k(k, arr) {
+    let maxSum = 0,
+        windowSum = 0,
+        windowStart = 0;
+    
+    for (let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+        windowSum += arr[windowEnd];
+
+        if (windowEnd >= k -1) {
+            maxSum = Math.max(maxSum, windowSum);
+            windowSum -= arr[windowStart];
+            windowStart += 1;
+        }
+    }
+    return maxSum;
+}

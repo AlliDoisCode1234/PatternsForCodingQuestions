c789c17ec03bced197018320de36b6a54f4ce52a
Josiah <josiah.james.fowler@gmail.com>
2021-07-13 10:57:17 -0400

    binary

--- 1_Sliding-Window/Practice/longestSubarray.js
+++ 1_Sliding-Window/Practice/longestSubarray.js
@@ -20,5 +20,5 @@ const length_of_longest_substring = (arr, k) => {
     return maxLength;
 }
 
-console.log(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1,] 2));
-console.log(length_of_longest_substring([0, 1, 0, 0,1,1,0,1,1,0,0,1,1,] 3))
+// console.log(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1,] 2));
+// console.log(length_of_longest_substring([0, 1, 0, 0,1,1,0,1,1,0,0,1,1,] 3))

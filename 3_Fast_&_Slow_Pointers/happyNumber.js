ade0efe376a39c36885ad3f3876ca72b64eaef08
Josiah <josiah.james.fowler@gmail.com>
2021-08-23 05:44:37 -0400

    happy number explained

--- 3_Fast_&_Slow_Pointers/happyNumber.js
+++ 3_Fast_&_Slow_Pointers/happyNumber.js
@@ -17,7 +17,7 @@ function find_happy_number(num) {
 function find_square_sum(num) {
     let sum = 0;
     while ((num>0)) {
-        digit = num % 10;
+        let digit = num % 10;
         sum += digit * digit;
         num = Math.floor(num / 10);
     }
@@ -26,3 +26,18 @@ function find_square_sum(num) {
 
 console.log(find_happy_number(23));
 console.log(find_happy_number(12));
+
+
+// taking in a Number, we are to find if it is a Happy Number
+// we initialize a fast and slow pointer to the Number
+// create a while loop to run while true (until we break out)
+// in our while loop, we move our slow pointer one step and our fast pointer two steps w/ a helper function
+// if slow and fast meet (equal each other) break out of while loop
+// after while loop return slow === 1 to see if the cycle is stuck on the number 1
+// Our helper function is then declared and we store sum in a variable
+// while the num we pass in the helper function is greater than 0
+// declare digit variable to equal num modulus 10
+// add digit multiply digit to sum
+// let num = the floor of our num divided by 10
+// outside of our while loop we return sum;
+
